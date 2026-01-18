9de145b6755c7c3a748e286147c626dddb990a12
ELC1123 <ethan_lester_l_chan@dlsu.edu.ph>
2026-01-18 14:21:21 +0800

    added AddCity functionality

--- src/main/java/com/elcprojects/citymanager/controller/WebController.java
+++ src/main/java/com/elcprojects/citymanager/controller/WebController.java
@@ -1,9 +1,12 @@
 package com.elcprojects.citymanager.controller;
 
+import com.elcprojects.citymanager.domain.City;
 import com.elcprojects.citymanager.service.CityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
@@ -18,5 +21,17 @@ public class WebController {
         var cities = cityService.findAll();
         model.addAttribute("cities", cities);
         return "cities";
+    }
+
+    @GetMapping("/add-city")
+    public String showAddCityForm(Model model) {
+        model.addAttribute("city", new City());
+        return "add-city";
+    }
+
+    @PostMapping("/save-city")
+    public String saveCity(@ModelAttribute City city) {
+        cityService.saveCity(city);
+        return "redirect:/";
     }
 }
